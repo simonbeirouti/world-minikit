94b4606923586a59ae92dbe2c57d4ea415fd9ce9
simonbeirouti <hello@simonbeirouti.com>
2024-11-07 01:16:32 +0700

    test on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { RouterScreen } from "@/components/RouterScreen";
+// import { RouterScreen } from "@/components/RouterScreen";
+import { OnboardingFlow } from "@/components/OnboardingFlow";
 
 export default function Home() {
   return (
     <main>
-      <RouterScreen />
+      <OnboardingFlow />
     </main>
   );
 }
