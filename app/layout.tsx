d781fc02c0a240667567180c89c306c2a0c19416
simonbeirouti <hello@simonbeirouti.com>
2024-11-08 01:11:21 +0700

    rv footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import MiniKitProvider from "@/components/minikit-provider";
 import dynamic from "next/dynamic";
 import NextAuthProvider from "@/components/next-auth-provider";
 import { Gate } from "@/components/gate";
-import {Footer} from "@/components/footer";
 import {Toaster} from "@/components/ui/toaster";
 
 const inter = Inter({subsets: ["latin"]});
@@ -28,13 +27,12 @@ export default function RootLayout({
 	);
 	return (
 		<html lang="en">
-			<body className={`${inter.className} h-screen w-screen overflow-hidden`}>
+			<body className={`${inter.className} h-screen w-screen`}>
 				<NextAuthProvider>
 					<ErudaProvider>
 						<MiniKitProvider>
 							<Gate>
 								{children}
-								<Footer />
 							</Gate>
 						</MiniKitProvider>
 					</ErudaProvider>
